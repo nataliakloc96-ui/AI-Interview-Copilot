0cd271aebc5fcec0108439aaf1c777432835e120
Natalia <6991nk@gmail.com>
2026-05-26 00:35:06 +0200

    Hard reset table schema

--- backend/init_db.py
+++ backend/init_db.py
@@ -2,16 +2,21 @@ from db import get_conn
 
 
 def init_db():
+
     conn = get_conn()
     cur = conn.cursor()
 
+    cur.execute("DROP TABLE IF EXISTS users")
+
     cur.execute("""
-        ALTER TABLE users
-        RENAME COLUMN password TO password_hash
+        CREATE TABLE users (
+            id SERIAL PRIMARY KEY,
+            email TEXT UNIQUE NOT NULL,
+            password_hash TEXT NOT NULL
+        )
     """)
 
     conn.commit()
-
     cur.close()
     conn.close()
 
