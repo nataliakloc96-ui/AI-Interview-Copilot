b70bd4d6d5cbed6c9dd6f3963578e48e0d95e108
Natalia <6991nk@gmail.com>
2026-05-26 00:03:33 +0200

    Add database adress

--- backend/db.py
+++ backend/db.py
@@ -3,5 +3,5 @@ import psycopg2
 
 def get_conn():
     return psycopg2.connect(
-        os.environ["DATABASE_URL"]
+        os.environ["postgresql://ai_interview_copilot_user:yG2DCDf4tHBc1Pn63iYRTixHEucEzxjW@dpg-d8accqlckfvc739coj20-a.frankfurt-postgres.render.com/ai_interview_copilot"]
     )
